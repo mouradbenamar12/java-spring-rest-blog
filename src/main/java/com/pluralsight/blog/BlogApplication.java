8ff8ce0bd143379130d3f1549f3c1723ea305285
Mourad Benamar <mouradbenamar12@gmail.com>
2022-07-09 22:19:01 +0100

    test for command liner

--- src/main/java/com/pluralsight/blog/BlogApplication.java
+++ src/main/java/com/pluralsight/blog/BlogApplication.java
@@ -1,13 +1,18 @@
 package com.pluralsight.blog;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BlogApplication {
+public class BlogApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BlogApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Project running");
+	}
 }
